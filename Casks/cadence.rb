dbef9d5ba74e1c618cc8fd5c768864eec7bb8377
Baptiste Arnaud <baptiste@typebot.io>
2026-05-14 15:41:14 +0200

    Update Cadence to 0.1.2

--- Casks/cadence.rb
+++ Casks/cadence.rb
@@ -1,9 +1,9 @@
 cask "cadence" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.1.1"
-  sha256 arm:   "879bc39aa4502c3e453d892c060356f38eb5a549870b2b23b28f5661b725978c",
-         intel: "fb11c21c6192ff6cbdd80d83c92e79a60394a0fbeb8772940c7d15cd2f3b59df"
+  version "0.1.2"
+  sha256 arm:   "620d8a19af9c12a521e14356c43648777b404e15d4059d214573a628f7e8f7c1",
+         intel: "271f3b1a136c6f4b0b563db0fbe36c535e26d846677ac08b6eea94c20479f8c2"
 
   url "https://github.com/baptisteArno/cadence/releases/download/cadence-v#{version}/Cadence-#{version}-#{arch}.dmg"
   name "Cadence"
