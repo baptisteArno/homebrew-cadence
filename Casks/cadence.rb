70c7fd43f5bc772b3ffd70d9454f91b3f2b3a624
Baptiste Arnaud <baptiste@typebot.io>
2026-05-14 15:23:50 +0200

    Fix Cadence 0.1.1 arm64 checksum

--- Casks/cadence.rb
+++ Casks/cadence.rb
@@ -2,7 +2,7 @@ cask "cadence" do
   arch arm: "arm64", intel: "x64"
 
   version "0.1.1"
-  sha256 arm:   "c436853228bcf26685243515ea9ec83b0901325be5d259c9f4cd4463664af6fd",
+  sha256 arm:   "879bc39aa4502c3e453d892c060356f38eb5a549870b2b23b28f5661b725978c",
          intel: "fb11c21c6192ff6cbdd80d83c92e79a60394a0fbeb8772940c7d15cd2f3b59df"
 
   url "https://github.com/baptisteArno/cadence/releases/download/cadence-v#{version}/Cadence-#{version}-#{arch}.dmg"
